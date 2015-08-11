e6bbdcc3488f13c8ae0b901b8998991ca631773a
Raul Adriano Ramos <raul.adriano.ramos@gmail.com>
2015-08-10 21:06:00 -0300

    aula 10/08/2015
    Nave e movimentacao

--- core/src/br/grupointegrado/SpaceInvaders/TelaJogo.java
+++ core/src/br/grupointegrado/SpaceInvaders/TelaJogo.java
@@ -1,13 +1,19 @@
 package br.grupointegrado.SpaceInvaders;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.utils.SpriteDrawable;
 import com.badlogic.gdx.utils.viewport.FillViewport;
 
 /**
@@ -24,6 +30,12 @@ public class TelaJogo extends TelaBase {
     private Stage palco;
     private BitmapFont fonte;
     private Label lbPontuacao;
+    private Image jogador;
+    private Texture texturaJogador;
+    private Texture texturaJogadorDireita;
+    private Texture texturaJogadorEsquerda;
+    private boolean indoDireita;
+    private boolean indoEsquerda;
 
 
 
@@ -49,6 +61,27 @@ public class TelaJogo extends TelaBase {
 
         initFont();
         initInformacoes();
+        initJogador();
+
+
+    }
+
+    /**
+     * inicia o jogador
+     */
+    private void initJogador() {
+        texturaJogador = new Texture("sprites/player.png");
+        texturaJogadorDireita = new Texture("sprites/player-right.png");
+        texturaJogadorEsquerda = new Texture("sprites/player-left.png");
+
+        jogador = new Image(texturaJogador);
+
+        float x = camera.viewportWidth/2 - jogador.getWidth()/2;
+        float y = 15;
+
+        jogador.setPosition(x, y);
+
+        palco.addActor(jogador);
 
 
     }
@@ -84,12 +117,66 @@ public class TelaJogo extends TelaBase {
         Gdx.gl.glClearColor(.15f,.15f, .25f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        lbPontuacao.setPosition(10,camera.viewportHeight-40);
+        lbPontuacao.setPosition(10, camera.viewportHeight - 40);
+        capturaTeclas();
+        atualizarJogador(delta);
+
+
         palco.act(delta);
         palco.draw();
 
 
 
+    }
+
+    /**
+     * atualiza a velocidade do jogador
+     * @param delta
+     */
+    private void atualizarJogador(float delta) {
+        float velocidade = 200; //velocidade de movimento do jogador
+        if (indoDireita){
+            if(jogador.getX() < camera.viewportWidth - jogador.getWidth()){
+                float x = jogador.getX()+ velocidade * delta;
+                float y = jogador.getY();
+                jogador.setPosition(x,y);
+            }
+
+
+
+
+
+        }
+        if (indoEsquerda){
+            if(jogador.getX() > 0) {
+                float x = jogador.getX() - velocidade * delta;
+                float y = jogador.getY();
+                jogador.setPosition(x, y);
+            }
+        }
+
+        if(indoDireita){
+            jogador.setDrawable(new SpriteDrawable(new Sprite(texturaJogadorDireita)));
+        }else if(indoEsquerda){
+            jogador.setDrawable(new SpriteDrawable(new Sprite(texturaJogadorEsquerda)));
+        }else{
+            jogador.setDrawable(new SpriteDrawable(new Sprite(texturaJogador)));
+        }
+    }
+
+    /**
+     * verifica se as teclas est'ao pressionadas
+     */
+    private void capturaTeclas() {
+        indoDireita = false;
+        indoEsquerda = false;
+
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)){
+            indoEsquerda = true;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
+            indoDireita = true;
+        }
     }
 
     /**
@@ -128,6 +215,9 @@ public class TelaJogo extends TelaBase {
         batch.dispose();
         palco.dispose();
         fonte.dispose();
+        texturaJogadorDireita.dispose();
+        texturaJogadorEsquerda.dispose();
+
 
     }
 }
